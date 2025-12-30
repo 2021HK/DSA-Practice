2d131d069dbb2895c742283133df1d031ba0030e
Harsh <137268461+2021HK@users.noreply.github.com>
2025-12-30 22:20:34 +0530

    Add isPalindrome function to check integer palindromes

---
+++ Basics _Problem/isPalindrom.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+using namespace std; 
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x <0 ) return false ;
+               int rev = 0 ; 
+               int dup = x;
+               
+               while(x!=0){
+                   int digit = x % 10;
+                   
+                     if(rev > INT_MAX/10) return false;
+                     
+                   rev = (rev*10)+digit;
+                   
+                   x = x/10;
+               }
+                return rev == dup;
+         }
+};
+
+int main() {
+    Solution sol;
+    int n;
+    cin >> n;
+    
+     if(sol.isPalindrome(n)) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
