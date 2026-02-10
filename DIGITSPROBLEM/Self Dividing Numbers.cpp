4a40f8e659cf1e86b2021ff0430364dabe112236
Harsh <137268461+2021HK@users.noreply.github.com>
2026-02-10 22:05:36 +0530

    Implement self-dividing numbers function

---
+++ DIGITSPROBLEM/Self Dividing Numbers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+         vector<int> ans;
+
+        for (int i = left; i <= right; i++) {
+            int num = i;
+            bool isSelfDividing = true;
+
+            while (num > 0) {
+                int digit = num % 10;
+
+                // zero not allowed
+                if (digit == 0 || i % digit != 0) {
+                    isSelfDividing = false;
+                    break;
+                }
+
+                num = num / 10;
+            }
+
+            if (isSelfDividing) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+    
+};
